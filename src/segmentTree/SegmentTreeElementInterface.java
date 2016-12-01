fa0a5039c78378300c2da69b1ac6f6e484af3fad
Andreas Østergaard Nielsen <kgoyo666@gmail.com>
2016-12-01 09:46:34 +0100

    init

---
+++ src/segmentTree/SegmentTreeElementInterface.java
@@ -0,0 +1,7 @@
+package segmentTree;
+
+/**
+ * Created by kgoyo on 01-12-2016.
+ */
+public interface SegmentTreeElementInterface {
+}
---
+++ src/segmentTree/SegmentTreeLeaf.java
@@ -0,0 +1,7 @@
+package segmentTree;
+
+/**
+ * Created by kgoyo on 01-12-2016.
+ */
+public class SegmentTreeLeaf implements SegmentTreeElementInterface {
+}
